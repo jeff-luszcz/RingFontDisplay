6a12160a7d7062a78a74892ba37b3daf56b1adf4
Jeff Luszcz <jeff@luszcz.com>
2020-09-17 00:28:37 +0000

    Update RingFontGraphvizGen.py

--- docs/RingFontGraphvizGen.py
+++ docs/RingFontGraphvizGen.py
@@ -1,51 +1,53 @@
-digraph g
-{
+# python script to create graphviz dot files to render the fonts
 
-layout=circo
-rankdir =TB;
+f = open("demofile2.txt", "w")
 
 
-rank1 [style=invis];
-rank2 [style=invis];
-rank1 -> rank2 [color=invis];
+f.write("digraph g\n")
+f.write("{\n")
+f.write("layout=circo\n")
+f.write("rankdir =TB;\n")
+f.write("rank1 [style=invis];\n")
+f.write("rank2 [style=invis];\n")
+f.write("rank1 -> rank2 [color=invis];\n")
+f.write("rank = same;\n")
+f.write("\n")
+f.write("\n")
+f.write("0 [fillcolor = red style=filled]\n")
+f.write("0 -> 1;\n")
+f.write("1->2;\n")
+f.write("2->3;\n")
+f.write("3->4;\n")
+f.write("4->5;\n")
+f.write("5->6;\n")
+f.write("6->7;\n")
+f.write("7->8;\n")
+f.write("8->9;\n")
+f.write("9->10;\n")
+f.write("10->11;\n")
+f.write("11->12;\n")
+f.write("12->13;\n")
+f.write("13->14;\n")
+f.write("14->15;\n")
+f.write("15->16;\n")
+f.write("16->17;\n")
+f.write("17->18;\n")
+f.write("18->19;\n")
+f.write("19->20;\n")
+f.write("20->21;\n")
+f.write("21->22;\n")
+f.write("22->23;\n")
+f.write("23->0;\n")
+f.write("rank2->6[style=invis];\n")
+f.write("\n")
+f.write("{rank=same; 2,rank2,5,12}\n")
+f.write("{rank=max; 0}\n")
+f.write("rankdir =TB;\n")
+f.write("\n")
+f.write("\n")
+f.write("0 [fillcolor = red style=filled]\n")
+f.write("\n")
+f.write("\n")
+f.write("}\n")
 
-
-rank = same;
-
-
-0 [fillcolor = red style=filled]
-0 -> 1;
-1->2;
-2->3;
-3->4;
-4->5;
-5->6;
-6->7;
-7->8;
-8->9;
-9->10;
-10->11;
-11->12;
-12->13;
-13->14;
-14->15;
-15->16;
-16->17;
-17->18;
-18->19;
-19->20;
-20->21;
-21->22;
-22->23;
-23->0;
-rank2->6[style=invis];
-
-{rank=same; 2,rank2,5,12}
-{rank=max; 0}
-rankdir =TB;
-
-
-0 [fillcolor = red style=filled]
-
-
-}
+f.close()
